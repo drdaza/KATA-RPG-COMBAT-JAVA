31d7b6a9221f1145b2b6fb2ddbdd6742647edf26
David Daza <drdaza18@gmail.com>
2023-01-10 11:30:21 +0100

    added first tests

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,5 +1,50 @@
 package com.factoriaf5.kata;
 
 public class Character {
+    private int Health;
+    private int Level;
+    private Boolean Alive;
+    private int Damage;
+
+    public Character() {
+        this.Health = 1000;
+        this.Level = 1;
+        this.Alive = true;
+        this.Damage = 10;
+    }
+
+    public int getHealth() {
+        return Health;
+    }
+    public void setHealth(int health) {
+        Health = health;
+    }
+    public int getLevel() {
+        return Level;
+    }
+    public void setLevel(int level) {
+        this.Level = level;
+    }
+    public Boolean getAlive() {
+        return Alive;
+    }
+    public void setAlive(Boolean alive) {
+        Alive = alive;
+    }
+    public int getDamage() {
+        return Damage;
+    }
+
+    public void setDamage(int damage) {
+        Damage = damage;
+    }
+    public void atackOtherCharacter(Character objetive){
+        objetive.setHealth(objetive.getHealth()-Damage);
+    }
+    public void YouDied(){
+        if(Health<=0) setAlive(false);
+    }
+
+    
     
 }
--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -10,5 +10,26 @@ public class CharacterTest {
     public void ExampleTest() {
         assertEquals(2, 2);
     }
+    @Test
+    public void All_characters_when_created_have(){
+        Character myCharacter = new Character();
+
+        assertEquals(1000, myCharacter.getHealth());
+        assertEquals(true, myCharacter.getAlive());
+        assertEquals(1, myCharacter.getLevel());
+    }
+    @Test
+    public void Characters_can_deal_damage_to_characters(){
+        Character myFirstCharacter = new Character();
+        Character mySecondCharacter = new Character();
+
+        myFirstCharacter.atackOtherCharacter(mySecondCharacter);
+        assertEquals(990, mySecondCharacter.getHealth());
+    }
+    @Test
+    public void Character_Die(){
+        
+    }
+
 
 }
